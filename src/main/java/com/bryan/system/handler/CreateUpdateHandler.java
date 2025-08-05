97efdbef0b216b26b8a00a084a74a73186263a09
Bryan Long <bryantaolong@outlook.com>
2025-08-05 21:00:09 +0800

    Fix MyBatis Plus MetaObjectHandler's implementation CreateUpdateHandler

--- src/main/java/com/bryan/system/handler/CreateUpdateHandler.java
+++ src/main/java/com/bryan/system/handler/CreateUpdateHandler.java
@@ -6,31 +6,40 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 /**
- * MyBatis-Plus 自动填充创建人/更新人处理器
+ * CreateUpdateHandler MyBatis-Plus 自动填充处理器
  *
  * @author Bryan Long
- * @since 2025/6/19 - 20:01
  * @version 1.0
+ * @since 2025/8/5
  */
 @Component
-public class CreateUpdateByHandler implements MetaObjectHandler {
+public class CreateUpdateHandler implements MetaObjectHandler {
 
     /**
+     * 在插入操作时自动填充 createTime 和 updateTime 字段
      * 在插入时填充 createBy 和 updateBy
      */
     @Override
     public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+
         String currentUsername = getCurrentUsername(); // 从上下文中获取当前用户
         this.strictInsertFill(metaObject, "createBy", String.class, currentUsername);
         this.strictInsertFill(metaObject, "updateBy", String.class, currentUsername);
     }
 
     /**
+     * 在更新操作时自动填充 updateTime 字段
      * 在更新时填充 updateBy
      */
     @Override
     public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+
         String currentUsername = getCurrentUsername(); // 从上下文中获取当前用户
         this.strictUpdateFill(metaObject, "updateBy", String.class, currentUsername);
     }
